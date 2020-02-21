f9376564422024f93e41ad055bb2993397c76ce5
seanaleid <seanaleid@gmail.com>
2020-02-20 20:35:48 -0500

    Create PlayerForm
      * Adds code for the basic structure of PlayerForm form and inputs
      * Adds inputHandler and submitHandler functions
      * Imports postPlayer action creator and passes it through connect

--- project/src/components/PlayerForm.js
+++ project/src/components/PlayerForm.js
@@ -1,13 +1,62 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
+import { postPlayer } from "../actions";
 
 const PlayerForm = (props) => {
     
+    const [player, setPlayer] = useState({
+        name: '',
+        rank: '',
+        nickname: ''
+    });
+       
+
+    const inputHandler = e => {
+        setPlayer({ [e.target.name]: e.target.value })
+    }
+
+    const submitHandler = e => {
+        e.preventDefault();
+        props.postPlayer(player);
+    }
+
     return (
         <div>
-            <h1>Hello from the PlayerForm component!!</h1>    
+            <form id="playerForm">
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    label="name"
+                    placeHolder="Lionel Messi"
+                    value={props.name}
+                    onChange={inputHandler}
+                    className="input"
+                />
+                <label htmlFor="rank">Rank</label>
+                <input
+                    type="text"
+                    name="rank"
+                    label="rank"
+                    placeHolder="1"
+                    value={props.rank}
+                    onChange={inputHandler}
+                    className="input"
+                />
+                <label htmlFor="nickname">Nickname</label>
+                <input
+                    type="text"
+                    name="nickname"
+                    label="nickname"
+                    placeHolder="La Pulga"
+                    value={props.nickname}
+                    onChange={inputHandler}
+                    className="input"
+                />
+                <button onClick={submitHandler} className="btn">Add a player</button>
+            </form>
         </div>
     )
 }
@@ -21,5 +70,5 @@ const mapStateToProps = (state) => {
 
 export default connect(
     mapStateToProps,
-    {}
+    { postPlayer }
 )(PlayerForm);
